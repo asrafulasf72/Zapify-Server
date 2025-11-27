dbc20105e21746ae1f5395aad43f461263a9a74c
asraful <asrafulasraful166@gmail.com>
2025-11-28 01:36:57 +0600

    Delete Api & Stripe Api Created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Password}@myfirst-cluster.32i1hy9.mongodb.net/?appName=myfirst-cluster`;
 
 
@@ -25,30 +27,76 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-       const db=client.db('ZapifyDB');
-       const parcelCollaction=db.collection('parcelsDB');
+    const db = client.db('ZapifyDB');
+    const parcelCollaction = db.collection('parcelsDB');
 
-       app.get('/parcels',async(req,res)=>{
-               const query={}
-               const {email}=req.query;
-               if(email){
-                query.senderEmail=email;
-               }
+    app.get('/parcels', async (req, res) => {
+      const query = {}
+      const { email } = req.query;
+      if (email) {
+        query.senderEmail = email;
+      }
 
-               const Options={sort: {createdAt: -1}}
-               const cursor= parcelCollaction.find(query, Options)
-              const  result= await cursor.toArray()
-               res.send(result)
-       });
+      const Options = { sort: { createdAt: -1 } }
+      const cursor = parcelCollaction.find(query, Options)
+      const result = await cursor.toArray()
+      res.send(result)
+    });
 
-       app.post('/parcels', async(req,res)=>{
-          const parcel=req.body
-          // Parcel Created Time
-          parcel.createdAt= new Date()
-          const result= await parcelCollaction.insertOne(parcel) 
-          res.send(result)  
-       });
+    app.get('/parcels/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await parcelCollaction.findOne(query)
+      res.send(result)
+    })
 
+    app.post('/parcels', async (req, res) => {
+      const parcel = req.body
+      // Parcel Created Time
+      parcel.createdAt = new Date()
+      const result = await parcelCollaction.insertOne(parcel)
+      res.send(result)
+    });
+
+
+    app.delete('/parcels/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await parcelCollaction.deleteOne(query)
+      res.send(result)
+    });
+
+    //  Payment Related Apis
+
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body
+      const amount = parseInt(paymentInfo.cost)*100
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+           price_data:{
+              currency:'USD',
+              unit_amount:amount,
+              product_data:{
+                name:paymentInfo.parcelName
+              }
+           },
+
+            quantity: 1,
+          },
+        ],
+        customer_email:paymentInfo.senderEmail,
+        mode: 'payment',
+        metadata:{
+           parcelId:paymentInfo.parcelId
+        },
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-canceled`,
+      })
+      console.log(session)
+      res.send({url: session.url})
+    });
 
 
 
